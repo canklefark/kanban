31d85dca2bf594a3e80c5f3753505571d5042639
Zeik <115008853+canklefark@users.noreply.github.com>
2024-07-23 22:22:35 -0400

    initial commit

---
+++ src/lib/components/navigation/Nav.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	import { AppRailTile, AppRailAnchor, AppRail } from '@skeletonlabs/skeleton';
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
+	import VscodeIconsFileTypeSvelte from '~icons/vscode-icons/file-type-svelte';
+	import VscodeIconsFileTypeTailwind from '~icons/vscode-icons/file-type-tailwind';
+	import VscodeIconsFileTypePostcss from '~icons/vscode-icons/file-type-postcss';
+
+	const drawerStore = getDrawerStore();
+	let currentTile: number = 0;
+
+	function drawerClose(): void {
+		drawerStore.close();
+	}
+</script>
+
+<!-- <nav class="list-nav p-4">
+	<ul>
+		<li><a href="/" on:click={drawerClose}>Homepage</a></li>
+		<li><a href="/about" on:click={drawerClose}>About</a></li>
+		<li><a href="/blog" on:click={drawerClose}>Blog</a></li>
+		<li><a href="/contact" on:click={drawerClose}>Contact</a></li>
+	</ul>
+</nav> -->
+<AppRail>
+	<svelte:fragment slot="lead">
+		<AppRailAnchor href="/" regionLabel="flex justify-center items-center">
+			<VscodeIconsFileTypeSvelte class="text-3xl" />
+		</AppRailAnchor>
+	</svelte:fragment>
+	<!-- --- -->
+	<AppRailTile
+		regionLead="flex justify-center items-center"
+		bind:group={currentTile}
+		name="tile-1"
+		value={0}
+		title="tile-1"
+	>
+		<svelte:fragment slot="lead">
+			<VscodeIconsFileTypeTailwind class="text-3xl" />
+		</svelte:fragment>
+		<span>Tile 1</span>
+	</AppRailTile>
+
+	<AppRailTile
+		regionLead="flex justify-center items-center"
+		bind:group={currentTile}
+		name="tile-2"
+		value={1}
+		title="tile-2"
+	>
+		<svelte:fragment slot="lead">
+			<VscodeIconsFileTypePostcss class="text-3xl" />
+		</svelte:fragment>
+		<span>Tile 2</span>
+	</AppRailTile>
+
+	<AppRailTile
+		regionLead="flex justify-center items-center"
+		bind:group={currentTile}
+		name="tile-3"
+		value={2}
+		title="tile-3"
+	>
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		<span>Tile 3</span>
+	</AppRailTile>
+	<!-- --- -->
+	<svelte:fragment slot="trail">
+		<AppRailAnchor
+			regionLead="flex justify-center items-center"
+			href="/"
+			target="_blank"
+			title="Account">(icon)</AppRailAnchor
+		>
+	</svelte:fragment>
+</AppRail>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppRail } from '@skeletonlabs/skeleton';
+	import { initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+	import Nav from '$lib/components/navigation/Nav.svelte';
+
+	// Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+	initializeStores();
+
+	const drawerStore = getDrawerStore();
+	function drawerOpen(): void {
+		drawerStore.open({});
+	}
+</script>
+
+<Drawer>
+	<h2 class="p-4">Navigation</h2>
+	<hr />
+	<Nav />
+</Drawer>
+<!-- App Shell -->
+<AppShell>
+	<!-- App Bar -->
+	<svelte:fragment slot="sidebarLeft">
+		<Nav />
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
